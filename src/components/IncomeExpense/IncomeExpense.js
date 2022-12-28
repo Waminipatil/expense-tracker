76bacc9e0cef5dedb966ecc6973d0beea1f92c47
Waminipatil <waminip@gmail.com>
2022-12-29 00:30:52 +0530

    Balance income & expense calculations

--- src/components/IncomeExpense/IncomeExpense.js
+++ src/components/IncomeExpense/IncomeExpense.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function IncomeExpense() {
+  const { transactions } = useContext(GlobalContext);
+  const income = transactions
+    .map((transaction) => transaction.amount > 0 && transaction.amount)
+    .reduce((acc, item) => (acc = acc + item));
+
+  const expense = Math.abs(
+    transactions
+      .map((transaction) => transaction.amount < 0 && transaction.amount)
+      .reduce((acc, item) => (acc = acc + item))
+  );
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+0.00</p>
+        <p className="money plus">{income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-0.00</p>
+        <p className="money minus">{expense}</p>
       </div>
     </div>
   );
